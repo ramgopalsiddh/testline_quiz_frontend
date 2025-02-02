f1e237323357953e8027c44983b2d9af5ac53e61
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2025-02-02 23:11:10 +0530

    Updated Featch directly

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,15 @@
 
-// CORS block this ddirect acces so i creat a python backend server and connect by that
-  const API_URL = "http://127.0.0.1:8000/quiz-data";
+export const fetchQuizData = async () => {
+  try {
+    const response = await fetch('https://api.jsonserve.com/Uw5CrX', {
+      mode: 'no-cors',
+    });
+    console.log(response)
+    if (!response.ok) throw new Error('Failed to fetch quiz data');
+    return await response.json(); // This will fail in `no-cors` mode
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
-  export const fetchQuizData = async () => {
-    try {
-      const response = await fetch(API_URL);
-      if (!response.ok) throw new Error("Failed to fetch quiz data");
-      return await response.json();
-    } catch (error) {
-      console.error(error);
-      return null;
-    }
-  };
